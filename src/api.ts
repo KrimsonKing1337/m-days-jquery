7d4ee8f324965037edfade85eee96d682be7d423
Oleg Shilov <shilov-1@yandex.ru>
2023-05-04 20:13:06 +0300

    added todo;

--- src/api.ts
+++ src/api.ts
@@ -16,9 +16,16 @@ export async function getCurrentWeather({ latitude, longitude }: getCurrentWeath
 
   console.log(`latitude: ${params.latitude}, longitude: ${params.longitude}`);
 
-  // если делать https запрос, у safari на старых ios возникает ошибка: не могу установить безопасное соединение
-  // поэтому, отрываем приложение через https, но запрос на погоду делаем через http
-  const result: AxiosResponse<WeatherResp> = await axios.get(`http://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+  /*
+  если делать https запрос, у safari на старых ios возникает ошибка: не могу установить безопасное соединение
+  поэтому, отрываем приложение через https, но запрос на погоду делаем через http
+
+  todo: но начиная с iphone 5 (проверить на iphone 4S) Сафари не разрешает заходить с https, но делать запросы
+   на http
+   попытаться определять по user-agent или ещё по каким-нибудь особенностям какой перед нами девайс и в зависимости
+   от этого запросы швырять либо на http, либо на https
+  */
+  const result: AxiosResponse<WeatherResp> = await axios.get('http://api.open-meteo.com/v1/forecast', { params });
 
   return result.data;
 }
