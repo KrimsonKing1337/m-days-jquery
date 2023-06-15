d43f094c0ecbde689e85423ad8c0287fe9dbd2a5
Oleg Shilov <shilov-1@yandex.ru>
2023-06-15 16:04:51 +0300

    removed click handler;

--- src/components/Bg/Bg.ts
+++ src/components/Bg/Bg.ts
@@ -2,16 +2,10 @@ import './Bg.scss';
 
 import { getRandomImgPath } from './utils';
 
-let timeout: ReturnType<typeof setTimeout> | null = null;
-
 $(document).ready(() => {
   const $bg = $('.js-bg');
   const $bgNext = $('.js-bg-next');
   const $animWrapper = $('.js-anim-wrapper');
-
-  const $extraWrapper = $('.js-extra-wrapper');
-  const $progressBar = $('.js-progress-bar');
-  const $mainQuestion = $('.js-main-question');
 
   let bgNext = getRandomImgPath();
 
@@ -40,15 +34,4 @@ $(document).ready(() => {
       changeOpacity('1');
     }, 700);
   }, 12000);
-
-  $extraWrapper.click(() => {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-
-    timeout = setTimeout(() => {
-      $progressBar.hide();
-      $mainQuestion.css('display', 'flex');
-    }, 60000 * 15);
-  });
 });
