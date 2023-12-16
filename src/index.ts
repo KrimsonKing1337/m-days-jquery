f0b148ae8f638f6429dcdc7a1a85f8ea03420c72
Oleg Shilov <shilov-1@yandex.ru>
2023-12-16 20:17:37 +0300

    added todo;

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import './vendors/jquery-1.9.1.min.js';
 import './vendors/bluebird.min.js';
 
 import './components/Bg/Bg';
-// todo: пересекаются css классы и css классы для js,
-//  из-за чего например progress-walking ломается в progress bar vaporwave
+// todo: пересекаются css классы и css классы для js.
+//  использовать либо БЭМ, либо css модули как-то прикрутить
 import './components/ProgressBar/ProgressBar';
 import './components/ProgressBarVaporwave/ProgressBarVaporwave';
 import './components/Weather/Weather';
