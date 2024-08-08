dca1f9b1d7af4e64f741a904a4f1522a040fbc3c
Oleg Shilov <shilov-1@yandex.ru>
2024-08-08 16:23:41 +0300

    added hidden menu with params;

--- src/components/Bg/Bg.ts
+++ src/components/Bg/Bg.ts
@@ -10,6 +10,11 @@ import { setSkin } from '../Weather/Weather';
 
 import imgBgJson from 'img_bg.json';
 
+type Checkbox = {
+  id: string;
+  label: string;
+};
+
 $(async () => {
   const rootElement = document.querySelector('#root');
 
@@ -25,6 +30,12 @@ $(async () => {
 
   const $progressBar = $('.js-progress-bar');
   const $progressBarCyberpunk = $('.js-progress-bar-vaporwave');
+
+  const $checkboxesWrapper = $('.js-checkboxes-wrapper');
+
+  const $menu = $('.js-menu');
+  const $menuButton = $('.js-menu-button');
+  const $menuButtonApply = $('.js-menu-button-apply');
 
   const searchParams = new URLSearchParams(window.location.search);
   const preset = searchParams.get('preset') || 'default';
@@ -65,6 +76,65 @@ $(async () => {
   } else {
     $progressBarCyberpunk.remove();
   }
+
+  const checkboxes: Checkbox[] = [
+    {
+      id: 'noPercent',
+      label: 'no percent',
+    },
+    {
+      id: 'noGui',
+      label: 'no gui',
+    },
+  ];
+
+  checkboxes.forEach((checkboxCur) => {
+    const { id, label } = checkboxCur;
+
+    const $wrapper = $('<div class="MenuCheckbox js-checkbox-wrapper"></div>');
+
+    const $label = $(`<label for="${id}">${label}</label>`);
+    const $checkbox = $(`<input id="${id}" type="checkbox" />`);
+
+    $wrapper.append($label);
+    $wrapper.append($checkbox);
+
+    $checkboxesWrapper.append($wrapper);
+  });
+
+  $menuButton.on('click', () => {
+    if ($menu.is(':hidden')) {
+      $menu.css('display', 'flex');
+    } else {
+      $menu.css('display', 'none');
+    }
+  });
+
+  $menuButtonApply.on('click', () => {
+    const noPercentValue = $('#noPercent').is(':checked');
+    const noGuiValue = $('#noGui').is(':checked');
+    const percentUpdate = $('#input-update-speed').val();
+
+    let params = '?';
+
+    if (noPercentValue) {
+      params += '&no-percent';
+    }
+
+    if (noGuiValue) {
+      params += '&no-gui';
+    }
+
+    if (percentUpdate !== '100') {
+      params += `&percent-update-speed=${percentUpdate}`;
+    }
+
+    if (params[1] === '&') {
+      params = params.replace('&', '');
+    }
+
+    window.location.href = `/${params}`;
+  });
 
   $progressBarsDull.hide();
   $progressBarsWrapper.show();
